ac4911f3ba98a687217c20e54eb8febbf948c86c
RendonSofia1 <rendonsofi03@gmail.com>
2024-05-28 18:51:35 -0600

    user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -13,10 +13,6 @@ export const getUser = async (request: Request, res: Response) => {
 };
 
 
-
-
-
-
 export const createUser = async (req: Request, res: Response) => {
     try { //evita que se detenga el servidor cuando falla el request
         console.log(req.body)
@@ -41,5 +37,41 @@ export const createUser = async (req: Request, res: Response) => {
 }
 
 
-// export const updateUser = async (request: Request, res: Response) => {
-// }
+export const updateUser = async (req: Request, res: Response) => {
+ 
+  try {
+      const { id } = req.params
+
+      const user = await User.findBy({ id: parseInt(req.params.id) })
+
+      if (!user)
+          return res.status(404).json({ message: "El usuario no existe" })
+
+      await User.update({ id: parseInt(id) }, req.body)
+
+      res.sendStatus(204)
+  } catch (error) {
+      if (error instanceof Error)
+          return res.status(500).json({ message: error.message })
+  }
+
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+
+  try {
+      const id = req.params.id
+
+      const result = await User.delete({ id: parseInt(id) })
+
+      if (result.affected == 0) {
+          res.status(404).json({ message: "Usuario no encontrado" })
+      }
+
+      return res.sendStatus(204)
+  } catch (error) {
+      if (error instanceof Error)
+          return res.status(500).json({ message: error.message })
+  }
+
+}
